71c57b76d1a0dbee4acb38fb7e8bc1a0ed3b89f4
Matias Tello <matiasfernandotello@gmail.com>
2024-06-03 22:10:37 -0300

    commit 5

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+
+
+
+const hamburg = document.querySelector('#hamburg');
+const linea1 = document.querySelector('#linea1');
+const linea2 = document.querySelector('#linea2');
+const linea3 = document.querySelector('#linea3');
+const nav = document.querySelector('#nav');
+
+
+
+// Btn abrir hamburguesa
+
+hamburg.addEventListener('click', ()=>{
+  // hamburg.classList.toggle('invisible');
+  
+  nav.classList.add('nav_visible');
+  linea1.classList.add('lin1');
+  linea2.classList.add('lin2');
+  linea3.classList.add('lin3');
+  console.log("hassdf");
+
+
+})
+
+// Función cerrar menú
+document.addEventListener('click', (e) => {
+    if (!nav.contains(e.target) && !hamburg.contains(e.target)) {
+      nav.classList.remove('nav_visible');
+      linea1.classList.remove('lin1');
+      linea2.classList.remove('lin2');
+      linea3.classList.remove('lin3');
+      
+      
+      // nav.classList.remove('active');
+  // nav.classList.remove('visible');
+    // hamburg.style.display= 'block';
+    }
+  });
