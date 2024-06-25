981239e6f5b6da239c21b82b3e7c8c3c59a0979b
Matias Tello <matiasfernandotello@gmail.com>
2024-06-25 20:39:10 -0300

    mediaqueries header_3

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -6,10 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
         img.addEventListener('click', function() {
             if (img.classList.contains('fullscreen')) {
                 img.classList.remove('fullscreen');
-                console.log("entro");
             } else {
                 img.classList.add('fullscreen');
-                console.log("no entro");
             }
         });
     });
